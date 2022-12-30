f7ee1958394ba4cbab5b702f1a54e9f97198407d
coderannu <akrajput01206@gmail.com>
2022-12-30 14:44:31 +0530

    update image

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Coderannu',
     title: "Full Stack Web Developer",
     desciption:"Every successful person in the world is a hustler one way or another. We all hustle to get where we need to be. Only a fool would sit around and wait on another man to feed him.",
-    image: 'https://github.com/CoderAnnu/portfolio/blob/master/src/assets/svg/contacts/default.jpg?raw=true',
+    image: 'https://raw.githubusercontent.com/CoderAnnu/coderannu.github.io/master/src/assets/svg/contacts/default.jpg',
     resumePdf: resume
 }
